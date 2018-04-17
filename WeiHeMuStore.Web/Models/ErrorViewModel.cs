0acd8e258271308bfbdfd4ec2d84cba362e8cb86
丶 Clearlove <gk1213656215@outlook.com>
2018-04-17 20:53:00 +0800

    添加项目文件。

---
+++ WeiHeMuStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeiHeMuStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
